d21042f2a27b178e6a59d5142908c91d131f7a0d
Diego <diegorealpet@gmail.com>
2025-11-29 00:27:50 -0600

    Attempting to give lambda api access again

--- amplify/functions/list-all-devices/handler.ts
+++ amplify/functions/list-all-devices/handler.ts
@@ -1,80 +1,38 @@
-export const handler = async (_event) => {
-  return "Hello from my first function!";
+import type { Schema } from '../../data/resource';
+import { Amplify } from 'aws-amplify';
+import { generateClient } from 'aws-amplify/data';
+import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime';
+
+const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig();
+
+Amplify.configure(resourceConfig, libraryOptions);
+
+const client = generateClient<Schema>();
+
+// Strongly-typed handler based on your schema
+export const handler: Schema['listAllDevices']['functionHandler'] = async (event) => {
+  const { farmId } = event.arguments;
+
+  // In this first iteration we don't care who the user is;
+  // both farmAdmin and tempViewer see the same list.
+  // (Later we'll plug in grants here.)
+  const result = await client.models.IoTDevice.list({
+    filter: {
+      farmId: { eq: farmId },
+    },
+  });
+
+  const devices = result.data ?? [];
+
+  // Map IoTDevice -> IoTDeviceView
+  const views: Schema['IoTDeviceView']['type'][] = devices.map((d) => ({
+    id: d.id,
+    type: d.type,
+    farmId: d.farmId,
+    devEui: d.devEui,
+    name: d.name ?? null,
+    location: d.location ?? null
+  }));
+
+  return views;
 };
-
-// import type { Schema } from '../../data/resource';
-// import { Amplify } from 'aws-amplify';
-// import { generateClient } from 'aws-amplify/data';
-// import outputs from "../../../amplify_outputs.json"
-
-// Amplify.configure(outputs);
-
-// const client = generateClient<Schema>();
-
-// // Strongly-typed handler based on your schema
-// export const handler: Schema['listAllDevices']['functionHandler'] = async (event) => {
-//   const { farmId } = event.arguments;
-
-//   // In this first iteration we don't care who the user is;
-//   // both farmAdmin and tempViewer see the same list.
-//   // (Later we'll plug in grants here.)
-//   const result = await client.models.IoTDevice.list({
-//     filter: {
-//       farmId: { eq: farmId },
-//     },
-//   });
-
-//   const devices = result.data ?? [];
-
-//   // Map IoTDevice -> IoTDeviceView
-//   const views: Schema['IoTDeviceView']['type'][] = devices.map((d) => ({
-//     id: d.id,
-//     type: d.type,
-//     farmId: d.farmId,
-//     devEui: d.devEui,
-//     name: d.name ?? null,
-//     location: d.location ?? null
-//   }));
-
-//   return views;
-// };
-// import type { Schema } from '../../data/resource';
-// import { Amplify } from 'aws-amplify';
-// import { generateClient } from 'aws-amplify/data';
-// // import outputs from "../../../amplify_outputs.json"
-// import { getAmplifyDataClientConfig } from '@aws-amplify/backend/function/runtime';
-// import { env } from '$amplify/env/list-all-devices'; // replace with your function name
-
-// const { resourceConfig, libraryOptions } = await getAmplifyDataClientConfig(env);
-
-// Amplify.configure(resourceConfig, libraryOptions);
-
-// const client = generateClient<Schema>();
-
-// // Strongly-typed handler based on your schema
-// export const handler: Schema['listAllDevices']['functionHandler'] = async (event) => {
-//   const { farmId } = event.arguments;
-
-//   // In this first iteration we don't care who the user is;
-//   // both farmAdmin and tempViewer see the same list.
-//   // (Later we'll plug in grants here.)
-//   const result = await client.models.IoTDevice.list({
-//     filter: {
-//       farmId: { eq: farmId },
-//     },
-//   });
-
-//   const devices = result.data ?? [];
-
-//   // Map IoTDevice -> IoTDeviceView
-//   const views: Schema['IoTDeviceView']['type'][] = devices.map((d) => ({
-//     id: d.id,
-//     type: d.type,
-//     farmId: d.farmId,
-//     devEui: d.devEui,
-//     name: d.name ?? null,
-//     location: d.location ?? null
-//   }));
-
-//   return views;
-// };
