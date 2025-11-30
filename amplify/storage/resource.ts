e9630aeebfd3d978b0970842f3ebebb45c653cce
Diego <diegorealpet@gmail.com>
2025-11-29 23:02:25 -0600

    Adjusting storage resource permissions

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -4,10 +4,20 @@ import { getFarmIotDataFn } from '../functions/get-farm-iot-data/resource';
 export const metricsBucket = defineStorage({
   name: 'metrics-bucket',
   access: (allow) => ({
-    '*': [
-      allow.guest.to(['read']), // optional – for public read
-      allow.authenticated.to(['read', 'write']), // optional
-      allow.resource(getFarmIotDataFn)
+    'raw/*': [
+      allow.guest.to(['read']),
+      allow.authenticated.to(['read', 'write']),
+      allow.resource(getFarmIotDataFn).to(['read', 'write', 'delete'])
+    ],
+    'parquet/*': [
+      allow.guest.to(['read']),
+      allow.authenticated.to(['read', 'write']),
+      allow.resource(getFarmIotDataFn).to(['read', 'write', 'delete'])
+    ],
+    'scripts/*': [
+      allow.guest.to(['read']),
+      allow.authenticated.to(['read', 'write']),
+      allow.resource(getFarmIotDataFn).to(['read', 'write', 'delete'])
     ],
   }),
 });
